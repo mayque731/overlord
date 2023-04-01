8554180d0632605531b5283d32ec1d345b4a7b13
mayque731 <mayque.731@gmail.com>
2023-03-31 22:43:54 -0400

    Merge branch 'master' of https://github.com/mayque731/overlord

--- lib/main.dart
+++ lib/main.dart
@@ -39,242 +39,207 @@ class MyApp extends StatelessWidget {
         ),
 
 //fim da barra
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              children: [
-                Image(
-                  width: 1000,
-                  image: AssetImage('assets/ainz.png'),
-                ),
-                Divider(
-                  height: 10.0,
-                ),
-                Center(
-                  child: Container(
-                    padding: const EdgeInsets.only(left: 40.0),
-                    alignment: Alignment.center,
-                    child: Column(
-                      children: [
-                        Text(
-                          "Character Status",
-                          style: TextStyle(
-                            fontFamily: "roboto",
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              "Health",
-                              style: TextStyle(
-                                fontFamily: "roboto",
-                                fontSize: 30,
-                              ),
-                            ),
-                            Icon(Icons.star, color: Colors.purple),
-                            Icon(Icons.star, color: Colors.purple),
-                            Icon(Icons.star, color: Colors.purple),
-                            Icon(Icons.star, color: Colors.purple),
-                            Icon(Icons.star, color: Colors.purple),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              "Mana",
-                              style: TextStyle(
-                                fontFamily: "roboto",
-                                fontSize: 30,
-                              ),
-                            ),
-                            Icon(Icons.star, color: Colors.purple),
-                            Icon(Icons.star, color: Colors.purple),
-                            Icon(Icons.star, color: Colors.purple),
-                            Icon(Icons.star, color: Colors.purple),
-                            Icon(Icons.star, color: Colors.purple),
-                            Icon(Icons.star, color: Colors.purple),
-                            Icon(Icons.star, color: Colors.purple),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              "Agility",
-                              style: TextStyle(
-                                fontFamily: "roboto",
-                                fontSize: 30,
-                              ),
-                            ),
-                            Icon(Icons.star, color: Colors.purple),
-                            Icon(Icons.star, color: Colors.purple),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              "Resistence",
-                              style: TextStyle(
-                                fontFamily: "roboto",
-                                fontSize: 30,
-                              ),
-                            ),
-                            Icon(Icons.star, color: Colors.purple),
-                            Icon(Icons.star, color: Colors.purple),
-                            Icon(Icons.star, color: Colors.purple),
-                            Icon(Icons.star, color: Colors.purple),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              "Physical Attack",
-                              style: TextStyle(
-                                fontFamily: "roboto",
-                                fontSize: 30,
-                              ),
-                            ),
-                            Icon(Icons.star, color: Colors.purple),
-                            Icon(Icons.star, color: Colors.purple),
-                            Icon(Icons.star, color: Colors.purple),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Divider(
-                  height: 10.0,
-                ),
-                Text(
-                  "About",
-                  style: TextStyle(
-                    fontFamily: "roboto",
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Container(
+        body: Center(
+          child: Column(
+            children: [
+              Image(
+                width: 1000,
+                image: AssetImage('assets/ainz.png'),
+              ),
+              Center(
+                child: Container(
                   child: Column(
                     children: [
                       Text(
-                        "''main protagonist of the Overlord series,",
-                        textAlign: TextAlign.center,
+                        "Character Status",
                         style: TextStyle(
                           fontFamily: "roboto",
                           fontSize: 25,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      Text(
-                        "He is the guildmaster of Ainz Ooal Gown,",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontFamily: "roboto",
-                          fontSize: 25,
-                        ),
+                      Row(
+                        children: [
+                          Text(
+                            "Health",
+                            style: TextStyle(
+                              fontFamily: "roboto",
+                              fontSize: 30,
+                            ),
+                          ),
+                          Icon(Icons.star, color: Colors.purple),
+                          Icon(Icons.star, color: Colors.purple),
+                          Icon(Icons.star, color: Colors.purple),
+                          Icon(Icons.star, color: Colors.purple),
+                          Icon(Icons.star, color: Colors.purple),
+                        ],
                       ),
-                      Text(
-                        "Overlord of the Great Tomb of Nazarick,",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontFamily: "roboto",
-                          fontSize: 25,
-                        ),
+                      Row(
+                        children: [
+                          Text(
+                            "Mana",
+                            style: TextStyle(
+                              fontFamily: "roboto",
+                              fontSize: 30,
+                            ),
+                          ),
+                          Icon(Icons.star, color: Colors.purple),
+                          Icon(Icons.star, color: Colors.purple),
+                          Icon(Icons.star, color: Colors.purple),
+                          Icon(Icons.star, color: Colors.purple),
+                          Icon(Icons.star, color: Colors.purple),
+                          Icon(Icons.star, color: Colors.purple),
+                          Icon(Icons.star, color: Colors.purple),
+                        ],
                       ),
-                      Text(
-                        "and the creator of Pandora's Actor,",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontFamily: "roboto",
-                          fontSize: 25,
-                        ),
+                      Row(
+                        children: [
+                          Text(
+                            "Agility",
+                            style: TextStyle(
+                              fontFamily: "roboto",
+                              fontSize: 30,
+                            ),
+                          ),
+                          Icon(Icons.star, color: Colors.purple),
+                          Icon(Icons.star, color: Colors.purple),
+                        ],
                       ),
-                      Text(
-                        "Highest of the Almighty Forty One Supreme Beings.''",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontFamily: "roboto",
-                          fontSize: 20,
-                        ),
+                      Row(
+                        children: [
+                          Text(
+                            "Resistence",
+                            style: TextStyle(
+                              fontFamily: "roboto",
+                              fontSize: 30,
+                            ),
+                          ),
+                          Icon(Icons.star, color: Colors.purple),
+                          Icon(Icons.star, color: Colors.purple),
+                          Icon(Icons.star, color: Colors.purple),
+                          Icon(Icons.star, color: Colors.purple),
+                        ],
                       ),
                     ],
                   ),
                 ),
-                Divider(
-                  height: 10.0,
-                ),
-                Text(
-                  "Habilities",
-                  style: TextStyle(
-                    fontFamily: "roboto",
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(20),
-                  alignment: Alignment.center,
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            ElevatedButton.icon(
-                              onPressed: () {},
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.purple,
-                                foregroundColor: Colors.white,
-                              ),
-                              icon: Icon(
-                                // <-- Icon
-                                Icons.heart_broken_sharp,
-                                size: 24.0,
-                              ),
-                              label: Text('Grasp Heart'),
-                              // <-- Text
-                            ),
-                            ElevatedButton.icon(
-                              onPressed: () {},
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.purple,
-                                foregroundColor: Colors.white,
-                              ),
-                              icon: Icon(
-                                // <-- Icon
-                                Icons.electric_bolt,
-                                size: 24.0,
-                              ),
-                              label: Text('Chain Light'),
-                              // <-- Text
-                            ),
-                          ],
-                        ),
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    Text(
+                      "Physical Attack",
+                      style: TextStyle(
+                        fontFamily: "roboto",
+                        fontSize: 30,
                       ),
-                    ],
-                  ),
+                    ),
+                    Icon(Icons.star, color: Colors.purple),
+                    Icon(Icons.star, color: Colors.purple),
+                    Icon(Icons.star, color: Colors.purple),
+                  ],
                 ),
-              ],
-            ),
-          ),
-        ),
-        drawer: Drawer(
-          child: Container(
-            color: Colors.black,
-            height: 50,
-            child: ListView(
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.purple,
-                    foregroundColor: Colors.white,
-                  ),
-                  child: Text("Test"),
+              ),
+              Text(
+                "About",
+                style: TextStyle(
+                  fontFamily: "roboto",
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
                 ),
-              ],
-            ),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Text(
+                      "''main protagonist of the Overlord series,",
+                      style: TextStyle(
+                        fontFamily: "roboto",
+                        fontSize: 25,
+                      ),
+                    ),
+                    Text(
+                      "He is the guildmaster of Ainz Ooal Gown,",
+                      style: TextStyle(
+                        fontFamily: "roboto",
+                        fontSize: 25,
+                      ),
+                    ),
+                    Text(
+                      "Overlord of the Great Tomb of Nazarick,",
+                      style: TextStyle(
+                        fontFamily: "roboto",
+                        fontSize: 25,
+                      ),
+                    ),
+                    Text(
+                      "and the creator of Pandora's Actor,",
+                      style: TextStyle(
+                        fontFamily: "roboto",
+                        fontSize: 25,
+                      ),
+                    ),
+                    Text(
+                      "Highest of the Almighty Forty One Supreme Beings.''",
+                      style: TextStyle(
+                        fontFamily: "roboto",
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                "Habilities",
+                style: TextStyle(
+                  fontFamily: "roboto",
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Row(
+                        children: [
+                          ElevatedButton.icon(
+                            onPressed: () {},
+                            icon: Icon(
+                              // <-- Icon
+                              Icons.person_add,
+                              size: 24.0,
+                            ),
+                            label: Text('Create Undead'),
+                            // <-- Text
+                          ),
+                          ElevatedButton.icon(
+                            onPressed: () {},
+                            icon: Icon(
+                              // <-- Icon
+                              Icons.heart_broken_sharp,
+                              size: 24.0,
+                            ),
+                            label: Text('Grasp Heart'),
+                            // <-- Text
+                          ),
+                          ElevatedButton.icon(
+                            onPressed: () {},
+                            icon: Icon(
+                              // <-- Icon
+                              Icons.electric_bolt,
+                              size: 24.0,
+                            ),
+                            label: Text('Chain Light'),
+                            // <-- Text
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
